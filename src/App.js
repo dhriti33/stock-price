878e4c4e30f1978353fb830560a9813cc2c5aae6
dhriti33 <61391304+dhriti33@users.noreply.github.com>
2020-06-21 16:33:06 -0700

    React again 1

--- src/App.js
+++ src/App.js
@@ -23,31 +23,29 @@ class App extends React.Component {
 }
 async componentDidMount() {
     this.getStock(this.state.li);
-    var Combined = new Array();
-      this.state.Combined[0] = ['Stock Name', 'Stock Value',{ role: "style" }];
-      var randomColor = require('randomcolor'); // import the script
-      var color = randomColor();
-      for (var i = 0; i < this.state.li.length; i++){
-        this.state.Combined[i + 1] = [this.state.li[i], this.state.stockData[i], `color: ${color}` ];
-        
-    }
+    //var Combined = new Array();
+    this.getGoogleArray()
     this.interval = setInterval(() => {
       this.getStock(this.state.li);
-      var Combined = new Array();
-      color = randomColor();
-      this.state.Combined[0] = ['Stock Name', 'Stock Value',{ role: "style" }];
-      for (var i = 0; i < this.state.li.length; i++){
-        this.state.Combined[i + 1] = [this.state.li[i], this.state.stockData[i], `color: ${color}`  ];    
-    }
+      this.getGoogleArray();
     }, 5000);
   }
+
+getGoogleArray(){
+      var randomColor = require('randomcolor'); 
+      let color = randomColor();
+      this.state.Combined[0] = ['Stock Name', 'Stock Value',{ role: "style" }];
+      for (var i = 0; i < this.state.li.length; i++){
+        this.state.Combined[i + 1] = [this.state.li[i], this.state.stockData[i], "color: #1337B7"  ];    
+    }
+}
 
 getStock= async (symbols) => {
     let term = "AAPL";
     let urllist=[]
     //let symbols=["AAPL","IBM","MSFT"]
     const key = 'brmkjn7rh5re15om60gg';
-    const url_finnhub="https://finnhub.io/api/v1/quote?symbol=AAPL&token=bq3klmvrh5rb0pdpe5ng"
+    
     for await (const sym of symbols){
       
         const url_finnhub=`https://finnhub.io/api/v1/quote?symbol=${sym}&token=bq3klmvrh5rb0pdpe5ng`
@@ -115,7 +113,8 @@ render() {
         </div>
         
         <div className="bar">
-        <Chart chartType="BarChart" width="100%" height="400px" data={this.state.Combined} />
+        
+        {this.state.Combined.length > 1 ? <Chart chartType="BarChart" width="100%" height="400px" data={this.state.Combined}/> : '' }
       </div>
           
       
